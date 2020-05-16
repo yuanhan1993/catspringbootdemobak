9fa1687f48bef458b5aa80aa368019112889dcbc
zhangjl16 <zhangjl16@asiainfo.com>
2020-05-16 15:39:27 +0800

    新增微信群组表

---
+++ src/main/java/com/qsls9/catspringbootdemo/model/GroupUser.java
@@ -0,0 +1,49 @@
+package com.qsls9.catspringbootdemo.model;
+
+public class GroupUser {
+    private Integer id ;
+    private Integer groupId ;
+    private String wxid;
+    private String robot_id;
+    private String wx_name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getWxid() {
+        return wxid;
+    }
+
+    public void setWxid(String wxid) {
+        this.wxid = wxid;
+    }
+
+    public String getRobot_id() {
+        return robot_id;
+    }
+
+    public void setRobot_id(String robot_id) {
+        this.robot_id = robot_id;
+    }
+
+    public String getWx_name() {
+        return wx_name;
+    }
+
+    public void setWx_name(String wx_name) {
+        this.wx_name = wx_name;
+    }
+}
