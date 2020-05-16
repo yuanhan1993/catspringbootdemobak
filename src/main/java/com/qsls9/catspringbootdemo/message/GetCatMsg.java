d04517102bdddb339966eaf3d0a14ab0afb55c96
zhangjl16 <zhangjl16@asiainfo.com>
2020-05-16 15:36:58 +0800

    新增微信群组表

--- src/main/java/com/qsls9/catspringbootdemo/message/GetCatMsg.java
+++ src/main/java/com/qsls9/catspringbootdemo/message/GetCatMsg.java
@@ -3,6 +3,8 @@ package com.qsls9.catspringbootdemo.message;
 import com.alibaba.fastjson.JSON;
 import com.qsls9.catspringbootdemo.model.FriendRequest;
 import com.qsls9.catspringbootdemo.service.FriendRequestService;
+import com.qsls9.catspringbootdemo.service.GroupService;
+import com.qsls9.catspringbootdemo.service.GroupUserService;
 import com.qsls9.catspringbootdemo.service.UserService;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +22,15 @@ public class GetCatMsg {
     private UserService userService;
     @Autowired
     private FriendRequestService friendRequestService;
+    @Autowired
+    private GroupService groupService;
+    @Autowired
+    private GroupUserService groupUserService;
+
     @RequestMapping("/sendMsg")
     @CrossOrigin(origins = "*")
     @ResponseBody
     public String getMsg(HttpServletRequest request) throws IOException, JSONException {
-        return M.send_demo(request,request.getParameter("type"),userService,friendRequestService);
+        return M.send_demo(request,request.getParameter("type"),userService,friendRequestService,groupService,groupUserService);
     }
 }
