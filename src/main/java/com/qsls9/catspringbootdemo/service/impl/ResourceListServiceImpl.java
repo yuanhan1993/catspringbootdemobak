410b3c5ceb7b2f9fec359b22ba6e2b01e9dca04c
zhangjl16 <zhangjl16@asiainfo.com>
2020-05-21 21:59:06 +0800

    发送资源列表

---
+++ src/main/java/com/qsls9/catspringbootdemo/service/impl/ResourceListServiceImpl.java
@@ -0,0 +1,36 @@
+package com.qsls9.catspringbootdemo.service.impl;
+
+import com.qsls9.catspringbootdemo.dao.ResourceListMapper;
+import com.qsls9.catspringbootdemo.model.ResourceList;
+import com.qsls9.catspringbootdemo.service.ResourceListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResourceListServiceImpl implements ResourceListService {
+
+    @Autowired
+    private ResourceListMapper resourceListMapper;
+    @Override
+    public List<ResourceList> select() {
+        return resourceListMapper.select();
+    }
+
+    @Override
+    public ResourceList selectById(Integer id) {
+        return resourceListMapper.selectById(id);
+    }
+
+    @Override
+    public Integer updatePassword(ResourceList resourceList) {
+        return resourceListMapper.updatePassword(resourceList);
+    }
+
+    @Override
+    public Integer selectCountById(Integer id) {
+        return resourceListMapper.selectCountById(id);
+    }
+
+}
