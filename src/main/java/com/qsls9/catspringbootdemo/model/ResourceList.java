9bca7804f5e2ff820ebc4313ce9fa3fc8100f825
zhangjl16 <zhangjl16@asiainfo.com>
2020-05-21 21:58:25 +0800

    发送资源列表

---
+++ src/main/java/com/qsls9/catspringbootdemo/model/ResourceList.java
@@ -0,0 +1,76 @@
+package com.qsls9.catspringbootdemo.model;
+
+public class ResourceList {
+    private Integer id ;
+    private String title;
+    private String link;
+    private String password;
+    private String imgurl;
+    private String restype;
+    private String createdate;
+    private Integer encryptflag;
+
+    public Integer getEncryptflag() {
+        return encryptflag;
+    }
+
+    public void setEncryptflag(Integer encryptflag) {
+        this.encryptflag = encryptflag;
+    }
+
+    public String getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(String createdate) {
+        this.createdate = createdate;
+    }
+
+    public String getRestype() {
+        return restype;
+    }
+
+    public void setRestype(String restype) {
+        this.restype = restype;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
