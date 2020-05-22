84f9001769b7d7ac1c6a31d4a0dcd2722ceef1c8
zhangjl16 <zhangjl16@asiainfo.com>
2020-05-22 19:56:12 +0800

    资源列表修改

--- src/main/java/com/qsls9/catspringbootdemo/service/ResourceListService.java
+++ src/main/java/com/qsls9/catspringbootdemo/service/ResourceListService.java
@@ -5,7 +5,8 @@ import com.qsls9.catspringbootdemo.model.ResourceList;
 import java.util.List;
 
 public interface ResourceListService {
-    List<ResourceList> select();
+    List<ResourceList> selectByType(String restype);
+
     ResourceList selectById(Integer id);
 
     Integer updatePassword(ResourceList resourceList);
